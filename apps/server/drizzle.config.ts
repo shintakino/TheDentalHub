3bcde29d52950fdb1bcea42a7ef3a77616b1407b
Shintakino <joshuajaynarvaza@gmail.com>
2025-09-24 03:20:32 +0800

    Feat: Setup Drizzle with supabase

--- apps/server/drizzle.config.ts
+++ apps/server/drizzle.config.ts
@@ -1,4 +1,6 @@
-import { defineConfig } from "drizzle-kit";
+import { config } from 'dotenv';
+import { defineConfig } from 'drizzle-kit';
+config({ path: '.env' });
 
 export default defineConfig({
 	schema: "./src/db/schema",
--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -1,3 +1,8 @@
-import { drizzle } from "drizzle-orm/node-postgres";
+import { config } from 'dotenv';
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
 
-export const db = drizzle(process.env.DATABASE_URL || "");
+config({ path: '.env' }); // or .env.local
+
+const client = postgres(process.env.DATABASE_URL!);
+export const db = drizzle({ client });
