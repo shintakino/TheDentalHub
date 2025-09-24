bf26f5f0f359ca461d39a3407d95464edc9247a1
Shintakino <joshuajaynarvaza@gmail.com>
2025-09-25 06:08:35 +0800

    Implement Google Provider in signin

--- apps/server/src/lib/auth.ts
+++ apps/server/src/lib/auth.ts
@@ -1,23 +1,30 @@
-import { betterAuth, type BetterAuthOptions } from "better-auth";
-import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "../db";
-import * as schema from "../db/schema/auth";
+import { betterAuth, type BetterAuthOptions } from 'better-auth';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { db } from '../db';
+import * as schema from '../db/schema/auth';
 
 export const auth = betterAuth<BetterAuthOptions>({
-	database: drizzleAdapter(db, {
-		provider: "pg",
+  database: drizzleAdapter(db, {
+    provider: 'pg',
 
-		schema: schema,
-	}),
-	trustedOrigins: [process.env.CORS_ORIGIN || ""],
-	emailAndPassword: {
-		enabled: true,
-	},
-	advanced: {
-		defaultCookieAttributes: {
-			sameSite: "none",
-			secure: true,
-			httpOnly: true,
-		},
-	},
+    schema: schema,
+  }),
+  trustedOrigins: [process.env.CORS_ORIGIN || ''],
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    // Add the Google provider here
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: 'none',
+      secure: true,
+      httpOnly: true,
+    },
+  },
 });
