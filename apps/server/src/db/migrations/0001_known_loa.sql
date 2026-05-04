67738bcf80346d0da413c13b0b815f8f6c54b252
Shintakino <joshuajaynarvaza@gmail.com>
2026-05-04 21:35:19 +0800

    Reset Codebase

--- apps/server/src/db/migrations/0001_known_loa.sql
+++
@@ -1,2 +0,0 @@
-CREATE TYPE "public"."user_role" AS ENUM('PATIENT', 'STAFF', 'OWNER', 'SUPER_ADMIN');--> statement-breakpoint
-ALTER TABLE "user" ADD COLUMN "role" "user_role" DEFAULT 'PATIENT' NOT NULL;
--- apps/server/src/db/schema/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './relations/userRelations';
-export * from './auth';
-export * from './branch';
-export * from './relations/branchRelations';
